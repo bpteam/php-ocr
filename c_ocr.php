63db8a5bd3616a5faf80259dec30569545c839f1
Evgeny Pynykh <bpteam22@gmail.com>
2013-05-17 01:32:58 +0300

    lib_parser-46 Разрезание текста на символы (Поиск строк с текстом)
    Поиск в тексте символы и знаки пробелов с переводами строк.

--- c_ocr.php
+++ c_ocr.php
@@ -19,12 +19,12 @@ class c_ocr
      * Массив рисунков символов для распознования
      * @var array array(GD)
      */
-    public static $img_letters;
+    public static $img_char;
     /**
      * Массив шаблонов символов для распознования
      * @var array array(GD)
      */
-    public static $img_letter_templates;
+    public static $img_char_templates;
 
     /**
      * Хранит информацию о изображении
@@ -36,13 +36,30 @@ class c_ocr
     public static $img_info;
 
     /**
-     * Фон ярче текста или нет
-     * true ярче
-     * false темнее
-     * @var bool
+     * [pix][X][Y]=Значение индекса цвета в пикселе XxY в изображении
+     * ['index'][index]=Количество цветов с таким индексом
+     * ['count_pix']=Количество пикселей в изображении
+     * ['percent'][index]= Процентное соотношение цветов на изображении
+     * @var array
      */
-    public static $background_brightness;
+    public static $colors_index;
 
+    /**
+     * Средняя яркасть рисунка
+     * @var float
+     */
+    public static $brightness_img;
+
+    /**
+     * Средняя яркость фона изображения
+     * @var float
+     */
+    public static $brightness_background;
+    /**
+     * Средняя яркость текста
+     * @var float
+     */
+    public static $brightness_text;
     /**
      * @param string $img_file Имя файла с исображением
      * @return int|resource
@@ -71,22 +88,26 @@ class c_ocr
                 }
                 break;
         }
-        //Сохраняем прозрачность, если есть
+        //Убираем прозрачность
         switch(self::$img_info[2])
         {
             case IMAGETYPE_GIF:
                 $transparent_index=imagecolortransparent($tmp_img);
                 if($transparent_index!==-1)
                 {
-                    $transparent_color=imagecolorsforindex($tmp_img, $transparent_index);
-                    $transparent_img_index=imagecolorallocate(self::$img, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
-                    imagecolortransparent(self::$img, $transparent_img_index);
-                    imagefill(self::$img, 0, 0, $transparent_img_index);
+                    //$transparent_color=imagecolorsforindex($tmp_img, $transparent_index);
+                    //$transparent_img_index=imagecolorallocate(self::$img, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
+                    //imagecolortransparent(self::$img, $transparent_img_index);
+                    //imagefill(self::$img, 0, 0, $transparent_img_index);
+                    $white=imagecolorallocate(self::$img, 255, 255, 255);
+                    imagefill(self::$img, 0, 0, $white);
                 }
                 break;
             case IMAGETYPE_PNG:
-                imagealphablending(self::$img, false);
-                imagesavealpha(self::$img, true);
+                //imagealphablending(self::$img, false);
+                //imagesavealpha(self::$img, true);
+                $white=imagecolorallocate(self::$img, 255, 255, 255);
+                imagefill(self::$img, 0, 0, $white);
                 break;
             default:
                 break;
@@ -97,40 +118,39 @@ class c_ocr
 
     /**
      * Подсчитываем количество цветов в изображении и их долю в палитре
+     * Сбор индексов цвета каждого пикселя
      * @return array
-     * ['index'][index]=Количество цветов с таким индексом
-     * ['count_pix']=Количество пикселей в изображении
-     * ['percent'][index]= Процентное соотношение цветов на изображении
+     *
      */
-    static function count_colors_indexs()
+    static function get_colors_index()
     {
-        $count_index_array['index']=array();
-
-        for($i=0;$i<self::$img_info[0];$i++)
+        self::$colors_index['index']=array();
+        for($x=0;$x<self::$img_info[0];$x++)
         {
-            for($j=0;$j<self::$img_info[1];$j++)
+            for($y=0;$y<self::$img_info[1];$y++)
             {
-                $pixel_index=imagecolorat(self::$img,$i,$j);
-                if(array_key_exists($pixel_index,$count_index_array['index']))
-                $count_index_array['index'][$pixel_index]++;
-                else $count_index_array['index'][$pixel_index]=1;
+                $pixel_index=imagecolorat(self::$img,$x,$y);
+                self::$colors_index['pix'][$x][$y]=$pixel_index;
+                if(array_key_exists($pixel_index,self::$colors_index['index']))
+                    self::$colors_index['index'][$pixel_index]++;
+                else self::$colors_index['index'][$pixel_index]=1;
             }
         }
-        arsort($count_index_array['index'],SORT_NUMERIC);
-        $count_index_array['count_pix']=self::$img_info[0]*self::$img_info[1];
-        foreach ($count_index_array['index'] as $key => $value)
+        arsort(self::$colors_index['index'],SORT_NUMERIC);
+        self::$colors_index['count_pix']=self::$img_info[0]*self::$img_info[1];
+        foreach (self::$colors_index['index'] as $key => $value)
         {
-            $count_index_array['percent'][$key]=($value/$count_index_array['count_pix'])*100;
+            self::$colors_index['percent'][$key]=($value/self::$colors_index['count_pix'])*100;
         }
-        return $count_index_array;
+        return self::$colors_index;
     }
 
     /**
-     * Вычисления тона фона изображения с текстом, Тон светлее текста или наоборот
+     * Вычисления цвета фона изображения с текстом, Фон светлее текста или наоборот, если темнее то цвета инвертируются цвета
      */
-    static function get_background_brightness()
+    static function check_background_brightness()
     {
-        $count_colors=self::count_colors_indexs();
+        $count_colors=self::get_colors_index();
         reset($count_colors['index']);
         $background_index=key($count_colors['index']);
         $background_color=imagecolorsforindex(self::$img, $background_index);
@@ -138,20 +158,92 @@ class c_ocr
         $mid_color['green']=0;
         $mid_color['blue']=0;
         // Собираем все цвета текста
+        $count_text_color=0;
         while(next($count_colors['index']))
         {
+            if($count_colors['percent'][key($count_colors['index'])]<1) continue;
+            $count_text_color++;
             $color_index=key($count_colors['index']);
             $color=imagecolorsforindex(self::$img, $color_index);
             $mid_color['red']+=$color['red'];
             $mid_color['green']+=$color['green'];
             $mid_color['blue']+=$color['blue'];
         }
-        $count=count($count_colors['index'])-1;// исключаем из количества цветов цвет фона
-        foreach ($mid_color as &$value) $value/=$count; //Вычисляем средний цвет текста
+        foreach ($mid_color as &$value) $value/=$count_text_color; //Вычисляем средний цвет текста
         unset($value);
-        if(($background_color['red']+$background_color['green']+$background_color['blue'])>($mid_color['red']+$mid_color['green']+$mid_color['blue']))
-            self::$background_brightness=true;
-        else self::$background_brightness=false;
-        return self::$background_brightness;
+        self::$brightness_background=($background_color['red']+$background_color['green']+$background_color['blue'])/3;
+        self::$brightness_text=($mid_color['red']+$mid_color['green']+$mid_color['blue'])/3;
+        if(self::$brightness_background<self::$brightness_text)
+        {
+            imagefilter(self::$img,IMG_FILTER_NEGATE);
+            self::get_colors_index();
+        }
     }
+
+    /**
+     * Разбивает ресунок с текстом на маленькие рисунки с символом
+     */
+    static function divide_char()
+    {
+
+    }
+
+    /**
+     *  Разбивает рисунок на строки с текстом
+     */
+    static function divide_to_line()
+    {
+        // Находим среднее значение яркости каждой пиксельной строки и всего рисунка
+        $brightness_lines=array();
+        self::$brightness_img=0;
+        for($y=0;$y<self::$img_info[1];$y++)
+        {
+            $brightness_lines[$y]=0;
+            for($x=0;$x<self::$img_info[0];$x++) $brightness_lines[$y]+=self::get_brightness_to_index(self::$colors_index['pix'][$x][$y]);
+            $brightness_lines[$y]/=self::$img_info[0];
+            self::$brightness_img+=$brightness_lines[$y];
+        }
+        self::$brightness_img/=self::$img_info[1];
+        $top_line=array();
+        $bottom_line=array();
+        //Находим все верхние и нижние границы строк текста
+        for($y=2;$y<self::$img_info[1]-3;$y++)
+        {
+            //Top
+            if($brightness_lines[$y-2]>self::$brightness_img &&
+               $brightness_lines[$y-1]>self::$brightness_img &&
+               $brightness_lines[$y]>self::$brightness_img &&
+               $brightness_lines[$y+1]<self::$brightness_img &&
+               $brightness_lines[$y+2]<self::$brightness_img &&
+               $brightness_lines[$y+3]<self::$brightness_img
+            )
+                $top_line[]=$y;
+            elseif($brightness_lines[$y-2]<self::$brightness_img &&
+                $brightness_lines[$y-1]<self::$brightness_img &&
+                $brightness_lines[$y]>self::$brightness_img &&
+                $brightness_lines[$y+1]>self::$brightness_img &&
+                $brightness_lines[$y+2]>self::$brightness_img
+            )
+                $bottom_line[]=$y;
+        }
+        $coord['top']=$top_line;
+        $coord['bot']=$bottom_line;
+        $red=imagecolorallocate(self::$img, 255, 0, 0);
+        $green=imagecolorallocate(self::$img, 0, 255, 0);
+        foreach ($top_line as $value) imageline(self::$img, 0, $value, self::$img_info[0], $value,$red);
+        foreach ($bottom_line as $value) imageline(self::$img, 0, $value, self::$img_info[0], $value,$green);
+
+        return $coord;
+    }
+
+    /**
+     * Вычисляем яркость цвета по его индексу
+     */
+    static function get_brightness_to_index($color_index,$img=null)
+    {
+        if($img===null) $img=self::$img;
+        $color=imagecolorsforindex($img, $color_index);
+        return ($color['red']+$color['green']+$color['blue'])/3;
+    }
+
 }
