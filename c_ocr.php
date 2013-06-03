8c720b27a529d1cb1dcd3d4a0133f08c3726abe1
Evgeny Pynykh <bpteam22@gmail.com>
2013-06-03 19:31:06 +0300

    lib_parser-50 Генерация шаблона из символа

--- c_ocr.php
+++ c_ocr.php
@@ -60,7 +60,12 @@ class c_ocr
         switch(self::$img_info[2])
         {
             case IMAGETYPE_PNG :
-                $tmp_img = imagecreatefrompng($img_file);
+                $tmp_img2=imagecreatefrompng($img_file);
+                $tmp_img=imagecreatetruecolor(self::$img_info[0], self::$img_info[1]);
+                $white=imagecolorallocate($tmp_img, 255, 255, 255);
+                imagefill($tmp_img, 0, 0, $white);
+                imagecopy($tmp_img, $tmp_img2, 0, 0, 0, 0, self::$img_info[0], self::$img_info[1]);
+                imagedestroy($tmp_img2);
                 break;
             case IMAGETYPE_JPEG :
                 $tmp_img = imagecreatefromjpeg($img_file);
@@ -77,8 +82,8 @@ class c_ocr
                 }
                 break;
         }
+        $tmp_img=self::check_background_brightness($tmp_img);
         imagecopy(self::$img, $tmp_img, 2, 2, 0, 0, self::$img_info[0], self::$img_info[1]);
-        self::$img=self::check_background_brightness(self::$img);
         return self::$img;
     }
 
@@ -125,15 +130,12 @@ class c_ocr
         $background_index=key($count_colors['index']);
         unset($count_colors['index'][$background_index]);
         // Собираем все цвета отличные от фона
-        if(self::$img_info[2]==IMAGETYPE_JPEG)
+        $background_brightness=self::get_brightness_to_index($background_index,$img);
+        $background_brightness=$background_brightness-($background_brightness*0.2);
+        foreach ($count_colors['index'] as $key => $value)
         {
-            $background_brightness=self::get_brightness_to_index($background_index,$img);
-            $background_brightness=$background_brightness-($background_brightness*0.2);
-            foreach ($count_colors['index'] as $key => $value)
-            {
-                $color_brightness=self::get_brightness_to_index($key,$img);
-                if($background_brightness<$color_brightness) unset($count_colors['index'][$key]);
-            }
+            $color_brightness=self::get_brightness_to_index($key,$img);
+            if($background_brightness<($color_brightness+50)) unset($count_colors['index'][$key]);
         }
         $indexes['text']=array_keys($count_colors['index']);
         $indexes['background']=$background_index;
@@ -152,7 +154,9 @@ class c_ocr
         $brightness_background=($background_color['red']+$background_color['green']+$background_color['blue'])/3;
         $mid_color=self::get_mid_color_to_indexes($img,$color_indexes['text']);
         $brightness_text=($mid_color['red']+$mid_color['green']+$mid_color['blue'])/3;
-        if($brightness_background<$brightness_text) imagefilter($img,IMG_FILTER_NEGATE); //Инвертируем если фон черный
+        if($brightness_background<$brightness_text) imagefilter($img,IMG_FILTER_NEGATE); //Инвертируем если фон темнее чем текст
+        $color_indexes=self::get_colors_index_text_and_background($img);
+        $img=self::chenge_color($img,$color_indexes['background'],255,255,255);
         return $img;
     }
 
@@ -199,8 +203,9 @@ class c_ocr
             $h_line=$bottom_line[$key]-$top_line[$key];
             if($h_min>$h_line) $h_min=$h_line;
         }
+
         // Увеличим все строки на треть самой маленькой для захвата заглавных букв м хвостов букв
-        $change_size=0.35*$h_min;
+        $change_size=0.3*$h_min;
         foreach ($top_line as $key => $value)
         {
             $top_line[$key]-=$change_size;
@@ -266,15 +271,21 @@ class c_ocr
                 $coordinates=self::coordinates_img($word_value,true,1);
                 $begin_char=$coordinates['start'];
                 $end_word=$coordinates['end'];
-                // Нарезаем на слова
+                // Нарезаем на символы
                 foreach ($begin_char as $begin_key => $begin_value)
                 {
-                    $img_char[$line_key][$word_key][]=imagecreatetruecolor($end_word[$begin_key]-$begin_value, $img_info['y']);
+                    $tmp_img=imagecreatetruecolor($end_word[$begin_key]-$begin_value, $img_info['y']);
+                    $white=imagecolorallocate($tmp_img, 255, 255, 255);
+                    imagefill($tmp_img, 0, 0, $white);
+                    imagecopy($tmp_img,$word_value,0,0,$begin_value,0,$end_word[$begin_key]-$begin_value,$img_info['y']);
+                    $w=imagesx($tmp_img);
+                    $coordinates_char=self::coordinates_img($tmp_img);
+                    $img_char[$line_key][$word_key][]=imagecreatetruecolor($w, $coordinates_char['end'][0]-$coordinates_char['start'][0]);
                     end($img_char[$line_key][$word_key]);
                     $key_array_word=key($img_char[$line_key][$word_key]);
                     $white=imagecolorallocate($img_char[$line_key][$word_key][$key_array_word], 255, 255, 255);
                     imagefill($img_char[$line_key][$word_key][$key_array_word], 0, 0, $white);
-                    imagecopy($img_char[$line_key][$word_key][$key_array_word],$word_value,0,0,$begin_value,0,$end_word[$begin_key]-$begin_value,$img_info['y']);
+                    imagecopy($img_char[$line_key][$word_key][$key_array_word],$tmp_img,0,0,0,$coordinates_char['start'][0],$w,$coordinates_char['end'][0]);
                 }
             }
         }
@@ -415,5 +426,72 @@ class c_ocr
         else $h=$img_info['y']*($w/$img_info['x']);
         imagecopyresampled($new_img, $img, 0, 0, 0, 0, $w, $h, $img_info['x'], $img_info['y']);
         return $new_img;
+    }
+
+    /**
+     * Изменение цвета в изображении, если изображение открыто через imagecreate,
+     * то можно просто поменять цвет индекса через функцию imagecolorset
+     * @param resource $img
+     * @param int $color_index индекс цвета который нужно изменить
+     * @param int $red
+     * @param int $green
+     * @param int $blue
+     * @return resource
+     */
+    static function chenge_color($img,$color_index,$red=0,$green=0,$blue=0)
+    {
+        $img_info['x']=imagesx($img);
+        $img_info['y']=imagesy($img);
+        $new_color=imagecolorallocate($img, $red, $green, $blue);
+        for($x=0;$x<$img_info['x'];$x++)
+        {
+            for($y=0;$y<$img_info['y'];$y++)
+            {
+                if(imagecolorat($img,$x,$y)==$color_index) imagesetpixel($img,$x,$y,$new_color);
+            }
+        }
+        return $img;
+    }
+
+    static function generate_template_char($img,$w=16,$h=16)
+    {
+        $img_info['x']=imagesx($img);
+        $img_info['y']=imagesy($img);
+        if($img_info['x']!=$w || $img_info['y']!=$h) $img=self::resize_img($img,$w,$h);
+        $color_indexes=self::get_colors_index_text_and_background($img);
+        $template_char=array();
+        for($y=0;$y<16;$y++)
+        {
+            $line='';
+            for($x=0;$x<16;$x++)
+            {
+                if(array_search(imagecolorat($img,$x,$y),$color_indexes['text'])!==false) $line.='1';
+                else $line.='0';
+            }
+            $template_char[]=$line;
+        }
+        return $template_char;
+    }
+
+    static function generate_template($chars,$imgs)
+    {
+        if(count($chars)!=count($imgs)) return false;
+        $tamplate=array();
+        foreach ($chars as $char_key => $char_value) $tamplate["{$char_value}"]=self::generate_template_char($imgs[$char_key]);
+        return $tamplate;
+    }
+
+    static function save_template($name,$template)
+    {
+        $json=json_encode($template,JSON_FORCE_OBJECT);
+        $fh=fopen($name,'w+');
+        fwrite($fh,$json);
+        fclose($fh);
+    }
+
+    static function load_template($name)
+    {
+        $json=file_get_contents($name);
+        return json_encode($json,true);
     }
 }
