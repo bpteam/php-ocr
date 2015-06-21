827973b53b665a5a2e692ac3aa12d1b11a9f8c79
bpteam <bpteam22@gmail.com>
2015-06-21 17:57:31 +0300

    comeback bootstrap :)

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+spl_autoload_register(function ($class) {
+
+    $prefix = 'bpteam\\';
+    $base_dir = __DIR__ . '/src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
